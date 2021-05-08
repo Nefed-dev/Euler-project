48c23443cf328f412c51608793b656161415e7d8
Nefedov Iurii <nefedovvw@yandex.ru>
2021-05-08 19:36:32 +0300

    Exercise 004 complete

--- euler_003.py
+++ euler_003.py
@@ -4,19 +4,7 @@
 #
 # The prime factors of 13195 are 5, 7, 13 and 29.
 # What is the largest prime factor of the number 600851475143 ?
-#
-# def get_prime_numbers(n):
-#     prime_numbers = []
-#     for number in range(2, n + 1):
-#         for prime in prime_numbers:
-#             if number % prime == 0:
-#                 break
-#         else:
-#             prime_numbers.append(number)
-#             print(number)
-#     return prime_numbers
-#
-# print(get_prime_numbers(600851475143))
+
 
 num = 600851475143
 count = 1
