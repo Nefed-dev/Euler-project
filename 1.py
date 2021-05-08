86f8a43165f70e44aa6b6ac85ffebfdd95376deb
Nefedov Iurii <nefedovvw@yandex.ru>
2021-05-08 16:21:34 +0300

    Complete 1st exercise

---
+++ 1.py
@@ -0,0 +1,15 @@
+# Если выписать все натуральные числа меньше 10, кратные 3 или 5,
+# то получим 3, 5, 6 и 9. Сумма этих чисел равна 23.
+# Найдите сумму всех чисел меньше 1000, кратных 3 или 5.
+
+# If we list all the natural numbers below 10 that are multiples of 3 or 5,
+# we get 3, 5, 6 and 9. The sum of these multiples is 23.
+# Find the sum of all the multiples of 3 or 5 below 1000.
+
+
+num = 0
+for x in range(1000):
+    if x % 3 == 0 or x % 5 == 0:
+        print(x)
+        num += x
+print(num)
