03da86e7e1bbd3b51bbd1d2bac81c6a2cc30262b
Iurii Nefedov <69164416+Nefed-dev@users.noreply.github.com>
2021-08-01 14:41:22 +0500

    Add euler 010 solution

---
+++ euler_010.py
@@ -0,0 +1,22 @@
+# Сумма простых чисел меньше 10 равна 2 + 3 + 5 + 7 = 17.
+# Найдите сумму всех простых чисел меньше двух миллионов.
+
+
+def get_primes(n):
+  m = n+1
+  numbers = [True] * m 
+  for i in range(2, int(n**0.5 + 1)):
+    if numbers[i]:
+      for j in range(i*i, m, i):
+        numbers[j] = False
+  primes = []
+  for i in range(2, m):
+    if numbers[i]:
+      primes.append(i)
+  return primes
+
+
+primes = get_primes(2000000)
+print(sum(primes))
+
+# Answer:142913828922
